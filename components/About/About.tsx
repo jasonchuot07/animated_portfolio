6179feecea8e4e7fe628d4871cd92de3fe392e4a
Jason <jasonchuot07@gmail.com>
2022-09-26 14:55:17 -0400

    Initiated coding the About section

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -4,8 +4,13 @@ type Props = {}
 
 const About = (props: Props) => {
   return (
-    <div>
-        
+    <div className='flex flex-col relative text-center md:text-left md:flex-row h-screen mx-w-7xl px-10 justify-evenly mx-auto items-center'>
+      <h3 className='absolute top-24 uppercase tracking-[20px] text-2xl'>About</h3>
+      <motion.img initial={{
+        x:-200
+      }} animate={{
+        x:0,
+      }} src='' />
     </div>
   )
 }
